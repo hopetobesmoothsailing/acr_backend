924d3ef065e67b261b37afda27c46dd5fcfeec38
wangyi85 <jackywang8911@gmail.com>
2023-11-09 14:41:09 +0800

    Port changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 const db = require('./model/db')
 const routes = require('./api/router')
 
