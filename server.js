d1b2727233bece229601b9c2df03d984079161bd
wangyi85 <jackywang8911@gmail.com>
2023-11-16 15:37:23 +0800

    cors policy added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000
 const db = require('./model/db')
 const routes = require('./api/router')
 
 app.use(express.urlencoded())
 app.use(express.json())
+app.use(cors());
 app.use(routes);
 
 app.listen(port, () => console.log(`Server is listening on Port ${port}`));
